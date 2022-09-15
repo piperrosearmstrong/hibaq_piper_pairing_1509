336de8c3a752626c91466900e2bfe860d33e4244
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-09-15 15:38:04 +0100

    Intermezzo Debugging 1 Challenge

--- lib/get_the_most_common_letter.rb
+++ lib/get_the_most_common_letter.rb
@@ -1,9 +1,14 @@
 def get_most_common_letter(text)
+# defines a new method that takes one parameter 'text'
   counter = Hash.new(0)
-  text.chars.each do |char|
+  # creates a new empty hash called 'counter' with a default value of zero
+  stripped_text = text.gsub(/[\s,!]/ ,"")
+  stripped_text.chars.each do |char|
+  # split our string into an array that we can loop through
     counter[char] += 1
   end
-  p counter.to_a.sort_by.max_by { |k, v| v }[0][0]
+  counter.to_a.sort.max_by { |k, v| v }[0][0]
+  # 
 end
   
   # Intended output:
