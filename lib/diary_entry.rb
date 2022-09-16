fe152ec7529e063b5725317a2842569f2680f331
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-09-16 17:19:47 +0100

    Diary Entry Complete

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -3,6 +3,7 @@ class DiaryEntry
 def initialize(title,contents)
     @title = title
     @contents = contents
+    @counter = 0
 end
 
 def title
@@ -14,14 +15,33 @@ def contents
 end
 
 def count_words
-   count = @contents.split.size
-   return count
+   words.count
 end
 
 def reading_time(wpm)
-   count = @contents.split.size
-   result = count / wpm
-   return result.to_f
+   (count_words / wpm.to_f).ceil
+   # to divide two integers that will have a decimal, convert one to_f 
+   # .ceil rounds up to the nearest integer
 end
+
+def reading_chunk(wpm, minutes)
+  words_read = minutes * wpm
+  chunk_start = @counter
+  chunk_end = words_read + @counter
+#   putting chunk_end into a variable means that we can update the end of the range after each time the method is run. 
+  chunk = words[chunk_start...chunk_end]
+# using counter in the range, means that it counts where we finished reading last time
+# currently my range is 0-4 (but not including 4), the counter should start reading from index 4('five')
+  @counter = chunk_end
+#   When we return this, it returns the counter
+  @counter = 0 if chunk_end >= count_words
+  chunk.join(" ")
+#   output value should be the last line in your method
+end
+
+private
+def words
+  @contents.split
+end   
 
 end
--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -24,8 +24,40 @@ end
 it "returns an integer representing an estimate of the reading time in minutes" do
     entry = DiaryEntry.new("wednesday","the internet stopped working")
     result = entry.reading_time(10)
-    expect(result).to eq 2.5
+    expect(result).to eq 1
 end
 
+it "it returns a chunk of the contents that the user can read in a specified amount of time" do
+  contents = "one two three four five six seven eight nine ten"
+#   put contents in separate variable for ease of use
+  entry = DiaryEntry.new("thursday",contents)
+  result = entry.reading_chunk(1, 4)
+  expect(result).to eq "one two three four"
+end    
+
+it "returns the second chunk of contents" do
+  contents = "one two three four five six seven eight nine ten"
+  entry = DiaryEntry.new("thursday",contents)
+  entry.reading_chunk(1, 4)
+#   this would return 'one two three four'
+  expect(entry.reading_chunk(1, 4)).to eq "five six seven eight"
+#   by putting this in the paranetheses, we're calling the method a second time
+end
+
+it "returns to the first chunk after the whole text has been read" do
+    contents = "one two three four five six seven eight nine ten"
+    entry = DiaryEntry.new("thursday",contents)
+    entry.reading_chunk(1, 10)
+    expect(entry.reading_chunk(1, 4)).to eq "one two three four"
+end
+
+it "returns to the first chunk after the whole text has been read" do
+    contents = "one two three four five six seven eight nine ten"
+    entry = DiaryEntry.new("thursday",contents)
+    entry.reading_chunk(1, 5)
+    entry.reading_chunk(1, 6)
+    expect(entry.reading_chunk(1, 4)).to eq "one two three four"
+end
 
 end
+
